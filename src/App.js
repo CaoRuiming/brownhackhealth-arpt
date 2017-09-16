87a034a39f8a88627855f8296239f3d364aa9981
Katie Hsia <katiehsia@Katies-MacBook-Air.local>
2017-09-16 16:39:21 -0400

    with header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Webcam from 'react-webcam';
 import './App.css';
 
 class App extends Component {
   render() {
 		const videoStyle = {
-			padding: 20,
 			transform: 'rotateY(180deg)',
-			'-webkit-transform': 'rotateY(180deg)', /* Safari and Chrome */
-			'-moz-transform': 'rotateY(180deg)', /* Firefox */
+			'WebkitTransform': 'rotateY(180deg)', /* Safari and Chrome */
+			'MozTransform': 'rotateY(180deg)', /* Firefox */
 		};
+    const overlayStyle = {
+       position:'absolute',
+    };
+
+    const video_src = "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4"
+    const img_src = "https://d2r1vs3d9006ap.cloudfront.net/s3_images/1156266/26621-17fv8xg_inline.png"
+    const width = window.innerWeight - 400
+    const height = window.innerHeight - 100
+
+    const tabs = {
+      width: width,
+      height: 60,
+      background: '#1959C9',
+      marginBottom: 20,
+    };
+
+    const logo = {
+      height: 50,
+      width: 50,
+      padding: 5,
+      marginRight: 20,
+      float: 'left',
+    };
+
+     const tab = {
+      height: 50,
+      width: 100,
+      padding: 5,
+      paddingTop:10,
+      fontSize: 30,
+      verticalAlign: 'center',
+      marginRight: 30,
+      float: 'left',
+     };
 
     return (
       <div className="App">
+        <div style={tabs}>
+          <img src={img_src} style={logo}/>
+          <div style={tab}>Workout</div>
+          <div style={tab}>Schedule</div>
+        </div>
+
 				<Webcam
 					audio={ false }
-					width={ window.window.innerWidth - 100 }
-					height={ window.window.innterheight - 100 }
+					width={ width }
+					height={ height }
 					style={ videoStyle }
 				/>
+
+
       </div>
     );
   }
